c6510b05381b98902e44fcba08cf74c016519886
Juliano-Dev <julianocg2013@gmail.com>
2021-09-25 01:30:56 -0300

    New version Project SpringBoot - Dashboar Sales

---
+++ backend/src/test/java/com/projetosds/dsvendas/DsvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetosds.dsvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
